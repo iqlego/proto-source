fc49890c40b4eb27a89968a23ae4e4e5d1c7fc84
goob30 <68452963+goob30@users.noreply.github.com>
2026-09-08 15:16:02 -0400

    updated hand button frame handling

--- src/handlers/btHandler.cpp
+++ src/handlers/btHandler.cpp
@@ -65,21 +65,73 @@ void BLEHandler::setOnFrameReceived(void (*callback)(const String& frame)) {
     L2099
     (header, button state [0 all off, 1 or 2 either button], battery as percentage)
 */
-void handleFrame(const String &frame) {
-    char frameHeader = frame[0];
-    switch(frameHeader) {
-        case 'L': // left hand
-            g.buttonState = frame[1] - '0';
-            return;
-        case 'R': // right hand
-            g.buttonState = frame[1] - '0' + 2;
-            return;
-        case 'P': // phone
-            return;
-        default: // add throw error perchance
-            return;
+
+char frameHeader = frame[0];
+char buttonValueChar = frame[1];
+
+int buttonValue = buttonValueChar - '0';
+
+int lastLeftButton = 0;
+int lastRightButton = 0;
+
+void setButtonIndex() {
+    bool isLeftHand = frameHeader == 'L' ? true : false;
+    if (isLeftHand && buttonValue != lastLeftButton) {
+        lastLeftButton = buttonValue;
+        g.buttonState = lastLeftButton;
+    }
+    else if (!isLeftHand && buttonValue != lastRightButton) {
+        lastRightButton = buttonValue;
+        g.buttonState = lastRightButton + 2;
     }
 }
+
+void handleFrame(const String &frame) {
+    if (frameHeader == 'L' || frameHeader == 'R') setButtonIndex();
+}
+
+// enum ActiveHandToParse {
+//     NONE,
+//     LEFT,
+//     RIGHT
+// };
+
+// ActiveHandToParse activeHandToParse;
+
+// char leftVal;
+// char rightVal;
+
+// void setButtonIndex(char id, char val) {
+    
+//     // if (id == 'L') {
+//     //     g.buttonState = frame[1] - '0';
+//     // }
+//     // else if (id == 'R') {
+//     //     
+//     // }
+//     // else {
+//     //     activeHandToParse = NONE;
+//     // }
+
+//     if (leftVal == 0 && rightVal == 0) g.buttonState == 0;
+    
+// }
+
+// void handleFrame(const String &frame) {
+//     char frameHeader = frame[0];
+//     char buttonValue = frame[1];
+//     // switch(frameHeader) {
+//     //     case 'L': // left hand
+//     //         return;
+//     //     case 'R': // right hand
+//     //         return;
+//     //     case 'P': // phone
+//     //         return;
+//     //     default: // add throw error perchance
+//     //         return;
+//     // }
+//     setButtonIndex(frameHeader, buttonValue);
+// }
 
 // header + helmet battery + l hand battery + r hand battery + current expression index + brightness level
 String createFrame(globals) {
